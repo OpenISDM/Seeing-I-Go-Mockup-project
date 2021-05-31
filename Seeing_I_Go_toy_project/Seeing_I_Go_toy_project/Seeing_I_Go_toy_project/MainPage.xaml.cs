80cf67ba9aa9987bb6769aaa5cfc4a93228030f0
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-05-31 13:28:33 +0800

    creat project

---
+++ Seeing_I_Go_toy_project/Seeing_I_Go_toy_project/Seeing_I_Go_toy_project/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Seeing_I_Go_toy_project
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
